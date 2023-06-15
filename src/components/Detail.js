ccd96c125a9332ca4b411f16523eb44e5cbaf92b
Hyung Kyu <onlymuk@naver.com>
2023-06-16 02:07:17 +0900

    fourth commit

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,20 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
-let YellowBtn = styled.button`
-   {
-    background: yellow;
-    color: black;
-    pading: 10px;
-  }
-`;
 
 const Detail = (props) => {
+  let [alert, setAlert] = useState(true);
+  let [num, setNum] = useState("");
+  useEffect(() => {
+    let a = setTimeout(() => setAlert(false), 2000);
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isNaN(num) === true) {
+      window.alert("숫자 아님");
+    }
+  }, [num]);
+
   let { id } = useParams();
   let findItem = props.shoes.find((x) => {
     return x.id == id;
   });
   return (
     <div className="container">
+      <input
+        onChange={(e) => {
+          setNum(e.target.value);
+        }}
+      />
+      {alert && (
+        <div className="alert alert-warning">2초 이내 구매 시 할인</div>
+      )}
       <div className="row">
         <div className="col-md-6">
           <img
